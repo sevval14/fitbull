615e1c9e88a9a606fb6d5059874a8a2f312f0d78
xnezya <byzylmzr1@gmail.com>
2023-12-05 22:16:44 +0300

    exlpore view completed

--- lib/screens/explore/view/explore_view.dart
+++ lib/screens/explore/view/explore_view.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/cupertino.dart';
+import 'package:fitbull/widgets/event_card.dart';
 import 'package:flutter/material.dart';
 
 class ExploreView extends StatefulWidget {
@@ -12,26 +12,83 @@ class _ExploreViewState extends State<ExploreView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        title: Row(
-          children: [
-            IconButton(
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              IconButton(
                 onPressed: () {},
                 icon: const Icon(
                   Icons.search,
                   color: Colors.black,
-                )),
-            const Expanded(
-                child: Text(
-                  "Gyms",
-                  style: TextStyle(color: Colors.black),
-                  textAlign: TextAlign.center,
-                ))
-          ],
+                ),
+              ),
+              const Expanded(
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text(
+                        "Gyms",
+                        style: TextStyle(color: Colors.black, fontSize: 18),
+                        textAlign: TextAlign.center,
+                      ),
+                      SizedBox(height: 2),
+                      Text(
+                        "Muğla, Turkey",
+                        style: TextStyle(color: Colors.black, fontSize: 14),
+                        textAlign: TextAlign.center,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
-      ),
-      body: ElevatedButton(onPressed: () {}, child: Text("sevval")),
-    );
+        body: Column(children: [
+          Expanded(
+            child: Image.asset(
+              "assets/home/img.png",
+              width: double.infinity, // Genişlik tam ekran genişliği kadar
+              height: double.infinity, // Yükseklik tam ekran yüksekliği kadar
+              fit: BoxFit.cover,
+            ),
+          ),
+          Expanded(
+              child: ExploreList(items: [
+                CardItem(
+                image: "assets/home/img_1.png",
+                name: "Acaademy",
+                address: "Kötekli mah.",
+                price: "90 TL", rating: "4.3"),
+                CardItem(
+                    image: "assets/home/img_1.png",
+                    name: "Acaademy",
+                    address: "Kötekli mah.",
+                    price: "90 TL",
+                    rating: "4.3"),
+                CardItem(
+                    image: "assets/home/img_1.png",
+                    name: "Acaademy",
+                    address: "Kötekli mah.",
+                    price: "90 TL",
+                    rating: "4.3"),
+                CardItem(
+                    image: "assets/home/img_1.png",
+                    name: "Acaademy",
+                    address: "Kötekli mah.",
+                    price: "90 TL",
+                    rating: "4.3"),
+                CardItem(
+                    image: "assets/home/img_1.png",
+                    name: "Acaademy",
+                    address: "Kötekli mah.",
+                    price: "90 TL",
+                    rating: "4.3"),
+          ]))
+        ]));
   }
 }
