6f82ae95ef785effe97385fa206bde294785fedc
sevval14 <sevvalozekinci85@gmail.com>
2024-03-24 17:59:27 +0300

    create path file for cleand code

---
+++ lib/services/service_path.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+enum ServicePath{
+  LOGIN,
+  REGISTER
+}
+extension ServicePathExtenion on ServicePath {
+  static const String _baseUrl = "http://192.168.73.68:8080";
+
+  String get path {
+    switch (this) {
+      case ServicePath.LOGIN:
+        return "$_baseUrl/auth/login";
+      case ServicePath.REGISTER:
+        return "$_baseUrl/auth/register";
+    }
+  }
+}
