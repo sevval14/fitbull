5b2777b2f3c120b45784c8787ad0af594de8ba9a
sevval14 <sevvalozekinci85@gmail.com>
2024-03-24 18:02:37 +0300

    collect all response in on file

---
+++ lib/services/response_message.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+String handleResponseRegister(int statusCode) {
+    switch (statusCode) {
+      case 400:
+        return "The email address is already in use.";
+      case 401:
+        return "Wrong information";
+      case 403:
+        return "Forbidden. Access is denied.";
+      case 404:
+        return "Not found. The resource doesn't exist.";
+      case 500:
+        return "Internal Server Error. Please try again later.";
+      default:
+        return "Registration failed with status code: $statusCode. Please try again.";
+    }
+
+}
+
+String handleResponseLogin(int statusCode) {
+  switch (statusCode) {
+    case 400:
+      return "Email system is not registered. Please sign up";
+    case 401:
+      return "Please check your information";
+    case 403:
+      return "Forbidden. Access is denied.";
+    case 404:
+      return "Not found. The resource doesn't exist.";
+    case 500:
+      return "Internal Server Error. Please try again later.";
+    default:
+      return "Registration failed with status code: $statusCode. Please try again.";
+  }
+
+}
