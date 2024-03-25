053ad0d8de3b99666939bf87ff954737cca5a807
sevval14 <sevvalozekinci85@gmail.com>
2024-03-25 03:07:04 +0300

    added gym endpoint

--- lib/services/service_path.dart
+++ lib/services/service_path.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 
 enum ServicePath{
   LOGIN,
-  REGISTER
+  REGISTER,
+  GYM,
 }
 extension ServicePathExtenion on ServicePath {
   static const String _baseUrl = "http://192.168.73.68:8080";
@@ -13,6 +14,8 @@ extension ServicePathExtenion on ServicePath {
         return "$_baseUrl/auth/login";
       case ServicePath.REGISTER:
         return "$_baseUrl/auth/register";
+      case ServicePath.GYM:
+        return "$_baseUrl/gym";
     }
   }
 }
