9477d37f98916011abaffc574588153f9b7686a8
sevval14 <sevvalozekinci85@gmail.com>
2024-04-02 04:37:13 +0300

    added new service path

--- lib/services/service_path.dart
+++ lib/services/service_path.dart
@@ -4,9 +4,10 @@ enum ServicePath{
   LOGIN,
   REGISTER,
   GYM,
+  ACTIVITES,
 }
 extension ServicePathExtenion on ServicePath {
-  static const String _baseUrl = "http://192.168.73.68:8080";
+  static const String _baseUrl = "http://192.168.91.68:8080";
 
   String get path {
     switch (this) {
@@ -16,6 +17,8 @@ extension ServicePathExtenion on ServicePath {
         return "$_baseUrl/auth/register";
       case ServicePath.GYM:
         return "$_baseUrl/gym";
+      case ServicePath.ACTIVITES:
+        return "$_baseUrl/activities";
     }
   }
 }
