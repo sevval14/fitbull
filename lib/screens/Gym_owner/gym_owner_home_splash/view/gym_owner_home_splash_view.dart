928525ef33d5ff54bed100b99a3c6920b2294475
xnezya <byzylmzr1@gmail.com>
2024-04-08 22:28:57 +0300

    adding sidebar

--- lib/screens/Gym_owner/gym_owner_home_splash/view/gym_owner_home_splash_view.dart
+++ lib/screens/Gym_owner/gym_owner_home_splash/view/gym_owner_home_splash_view.dart
@@ -1,5 +1,7 @@
-import 'package:fitbull/screens/Gym_owner/gym_owner_gym/view/gym_owner_gym_view.dart';
 import 'package:flutter/material.dart';
+
+import '../../create_gym/create_activity/view/create_activity_view.dart';
+import '../../gym_owner_profile/view/gym_owner_profile.dart';
 
 
 class GymOwnerHomeSplashView extends StatefulWidget {
@@ -10,59 +12,109 @@ class GymOwnerHomeSplashView extends StatefulWidget {
 }
 
 class _GymOwnerHomeSplashViewState extends State<GymOwnerHomeSplashView> with SingleTickerProviderStateMixin {
-  late TabController _tabController;
-
-  @override
-  void initState() {
-    super.initState();
-    _tabController = TabController(length: 3, vsync: this);
-  }
-
-  @override
-  void dispose() {
-    _tabController.dispose();
-    super.dispose();
-  }
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: TabBarView(
-        controller: _tabController,
-        children: [
-         GymOwnerGym(),
-          GymOwnerGym(),
-          GymOwnerGym(),
-
-
-          // Farklı sayfa eklemeleri
+      key: _scaffoldKey,
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.menu),
+          onPressed: () {
+            _scaffoldKey.currentState?.openDrawer();
+          },
+        ),
+        title: Text(
+          'Academy Fit',
+          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+        ),
+        centerTitle: true,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.notifications_none),
+            onPressed: () {
+              // Notification icon action
+            },
+          ),
         ],
       ),
-      bottomNavigationBar: Material(
-        color: Colors.white,
-        child: TabBar(
-          unselectedLabelStyle: TextStyle(color: Colors.grey),
-          labelStyle: TextStyle(color: Colors.black),
-          unselectedLabelColor: Colors.grey,
-          labelColor: Colors.black,
-          controller: _tabController,
-          tabs:const [
-            Tab(
-              icon: Icon(Icons.home),
-              text: "Home",
-
+      drawer: Drawer(
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              child: ListView(
+                // Important: Remove any padding from the ListView.
+                padding: EdgeInsets.zero,
+                children: <Widget>[
+                  DrawerHeader(
+                    decoration: BoxDecoration(
+                      color: Colors.blue,
+                    ),
+                    child: Text(
+                      'Fitbull',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 24,
+                      ),
+                    ),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.insert_chart),
+                    title: Text('Daily Statistics'),
+                    onTap: () {
+                      // Add your onTap code here
+                    },
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.create),
+                    title: Text('Create Activity'),
+                    onTap: () {
+                      Navigator.of(context).push(MaterialPageRoute(builder: (context) => CreateActivityPage()));
+                    },
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.school),
+                    title: Text('Add Educator'),
+                    onTap: () {
+                      // Add your onTap code here
+                    },
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.edit),
+                    title: Text('Edit Profile'),
+                    onTap: () {
+                      // Add your onTap code here
+                    },
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.settings),
+                    title: Text('Settings'),
+                    onTap: () {
+                      // Add your onTap code here
+                    },
+                  ),
+                  // Add other sections here
+                ],
+              ),
             ),
-            Tab(
-              icon: Icon(Icons.fitness_center),
-              text: "Create Activity",
-            ),
-            Tab(
-              icon: Icon(Icons.computer),
-              text: "Profile",
+            // Logout section
+            Align(
+              alignment: FractionalOffset.bottomLeft,
+              child: ListTile(
+                leading: Icon(Icons.exit_to_app),
+                title: Text('Logout'),
+                onTap: () {
+                  // Handle the logout tap
+                },
+              ),
             ),
           ],
         ),
       ),
+
+
+      body: ProfileScreen(), // Your ProfileScreen widget here
     );
   }
 }
